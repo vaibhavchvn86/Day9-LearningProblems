41eafe9bedfd80e7b0d0564533dce90014a38165
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-20 20:03:36 +0530

    Employee wage Solution File

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Day_9_LP_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1_EmpW.UC1();
+            UC2_EmpW.UC2();
+            UC3_EmpW.UC3();
+            UC4_EmpW.UC4();
+            UC5_EmpW.UC5();
+            UC6_EmpW.UC6();
+            UC7_EmpW.ComputeUC7();
+            UC8_EmpW.ComputeUC8("Dmart",20,10,8);
+        }
+    }
+}
