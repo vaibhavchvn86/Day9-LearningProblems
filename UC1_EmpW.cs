f83037ea87262a138025946e6ed4028966f74c23
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-20 19:57:26 +0530

    UC1 Employee wage

---
+++ UC1_EmpW.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_9_LP_assignment
+{
+    class UC1_EmpW
+    {
+        public static void UC1()
+        {
+            int Present = 1;
+            int absent = 2;
+            Random Attend = new Random();
+            int Attendance = Attend.Next(1, 3);
+            if (Attendance == 1)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+
+        }
+    }
+
