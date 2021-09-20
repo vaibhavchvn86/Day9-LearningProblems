6bbaf89b7a18494abfdf8881efaaf42272445951
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-20 20:00:15 +0530

    UC3 Employee wage

---
+++ UC3_EmpW.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_9_LP_assignment
+{
+    class UC3_EmpW
+    {
+        public static void UC3()
+        {
+            int EmpWagePerHour = 20;
+            int PartTimeHrs = 8;
+            int TotalDailyWage;
+            TotalDailyWage = EmpWagePerHour * PartTimeHrs;
+            Console.WriteLine("Employee daily Wage is " + TotalDailyWage);
+        }
+    }
+}
