86a45f46f4dad4f4c4bf9a250984ab81c7586ea6
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-20 19:58:59 +0530

    UC2 Employee wage

---
+++ UC2_EmpW.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_9_LP_assignment
+{
+    class UC2_EmpW
+    {
+        public static void UC2()
+        {
+            int EmpWagePerHour = 20;
+            int FulldayHour = 8;
+            int TotalDailyWage;
+            TotalDailyWage = EmpWagePerHour * FulldayHour;
+            Console.WriteLine("Employee daily Wage is " + TotalDailyWage);
+        }
+    }
+}
